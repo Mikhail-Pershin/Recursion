f38a2868f86dab281fc98c756d9480bf0f953913
Mikhail_Pershin <mcpershin@yandex.ru>
2023-04-11 10:58:47 +0300

    add solution of task 66

--- Program.cs
+++ Program.cs
@@ -1,11 +1,45 @@
-﻿// Задача 64: Выполнить с помощью рекурсии.Задайте значение N. 
-// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+﻿// // Задача 64: Выполнить с помощью рекурсии.Задайте значение N. 
+// // Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
 
-// N = 5 -> "5, 4, 3, 2, 1"
-// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+// // N = 5 -> "5, 4, 3, 2, 1"
+// // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 
-Console.Write("Введите число: ");
+// Console.Write("Введите число: ");
+// int N = int.Parse(Console.ReadLine()!);
+// if (N <= 0)
+// {
+//   Console.Write($"{N} не натуральное число");
+//   return;
+// }
+
+// string GetNaturalNumbers(int number)
+// {
+//   if (number == 1) return number.ToString();
+
+//   return number + ", " + GetNaturalNumbers(number - 1);
+// }
+
+// Console.Write($"Натуральные числа в промежутке от N до 1 -> ");
+// Console.Write(GetNaturalNumbers(number: N));
+
+
+
+// Задача 66: Выполнить с помощью рекурсии.Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Write("Введите первое число: ");
+int M = int.Parse(Console.ReadLine()!);
+if (M <= 0)
+{
+  Console.Write($"{M} не натуральное число");
+  return;
+}
+Console.Write("Введите второе число: ");
 int N = int.Parse(Console.ReadLine()!);
 if (N <= 0)
 {
@@ -13,12 +47,14 @@ if (N <= 0)
   return;
 }
 
-string GetNaturalNumbers(int number)
+int NaturalNumbersSum(int start, int end, int total)
 {
-  if (number == 1) return number.ToString();
-
-  return number + ", " + GetNaturalNumbers(number - 1);
+  if (start > end) return total;
+  else
+  {
+    total += start;
+    return NaturalNumbersSum(start + 1, end, total);
+  }
 }
-
-Console.Write($"Натуральные числа в промежутке от N до 1 -> ");
-Console.Write(GetNaturalNumbers(number: N));
+int total = NaturalNumbersSum(start: M, end: N, total: 0);
+Console.Write($"Сумма натуральных элементов в промежутке от {M} до {N} -> {total}");
